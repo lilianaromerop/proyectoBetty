31f76db5d688154c06c106edd96930bae5bdff07
lilianaromerop <lilianaromerop11@gmail.com>
2022-06-21 18:07:32 -0500

    pagina biografia

---
+++ pages/biografia.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout } from '../components/layout/Layout'
+
+const biografia = () => {
+  return (
+    <Layout>
+        <div>
+            hola
+        </div>
+    </Layout>
+  )
+}
+
+export default biografia
